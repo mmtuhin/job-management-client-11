ac8ad812cfec3dbf45a5b053840202b5fa1fb0fc
Tuhin Hossain <mmtuhinhossain@gmail.com>
2023-11-09 19:04:46 +0600

    Blogs added

--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -3,7 +3,35 @@
 const Blogs = () => {
     return (
         <div>
-            <h1>This is blogs page</h1>
+            <h1 className="text-center my-4">Access token and Refresh token</h1>
+            <div className="border max-w-3xl text-center mx-auto">
+            <p>An access token is a string representing an authorization issued to the client. Tokens represent specific scopes and duration of access, granted by the resource owner, and enforced by the resource server and authorization server.</p>
+            <p>The responsibility of refresh token is to request for a new access token when the existing access token is expired.</p>
+            <p>We should store it in a cookie in client side, it can be safer than local browser storage.</p>
+            </div>
+            <h1 className="text-center my-4">Express JS</h1>
+            <div className="border max-w-3xl text-center mx-auto">
+            <p>An access token is a string representing an authorization issued to the client. Tokens represent specific scopes and duration of access, granted by the resource owner, and enforced by the resource server and authorization server.</p>
+            <p>The responsibility of refresh token is to request for a new access token when the existing access token is expired.</p>
+            </div>
+            <h1 className="text-center my-4">Next JS</h1>
+            <div className="border max-w-3xl text-center mx-auto">
+
+            <p>Next.js is an open-source web development framework created by the private company Vercel providing React-based web applications with server-side rendering and static website generation.</p>
+            </div>
+
+            <h1 className="text-center my-4">My Code</h1>
+            <div className="border max-w-3xl text-center mx-auto mb-8">
+
+            <p>I have created a react router dom single page app. builds necessary components.</p>
+            <ul>
+                <li>Client Side- React</li>
+                <li>Server Sider- Express JS</li>
+                <li>Database- Mongodb</li>
+                <li>Framework- tailwind, Daisy UI</li>
+            </ul>
+            </div>
+            
         </div>
     );
 };
